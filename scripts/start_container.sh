e70a9066659c8c9d12f3eb3c642eaed4f80ac688
sangameshragate <sanguragate@gmail.com>
2025-12-20 00:20:31 +0530

    Update Docker image references in start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull sangameshragate/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 sangameshragate/simple-python-flask-app
+
